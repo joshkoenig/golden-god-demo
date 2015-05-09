4b7c1c7f2f8e32da3bbf4e51117a53cef348e234
Josh Koenig <josh@getpantheon.com>
2015-05-09 12:49:14 -0700

    Moving terminus around

--- private/scripts/install.sh
+++ private/scripts/install.sh
@@ -4,14 +4,14 @@ composer self-update
 composer install
 
 echo "Install terminus directly"
-curl https://github.com/pantheon-systems/cli/releases/download/0.5.5/terminus.phar -L -o /usr/local/bin/terminus 
-chmod +x /usr/local/bin/terminus
+curl https://github.com/pantheon-systems/cli/releases/download/0.5.5/terminus.phar -L -o terminus
+chmod +x terminus
 
 export PATH="$TRAVIS_BUILD_DIR/vendor/bin:$PATH"
 # Log in, but don't output the password to Travis
 echo "Authenticating into terminus"
 {
-  terminus auth login $PEMAIL --password=$PPASS
+  ./terminus auth login $PEMAIL --password=$PPASS
 } &> /dev/null
 
-terminus sites list
+./terminus sites list

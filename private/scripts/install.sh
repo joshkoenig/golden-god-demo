c673b9c4beceeb522a24c2f7149de2a4ea4ae323
Josh Koenig <josh@getpantheon.com>
2015-05-09 12:36:35 -0700

    Next steps with Travis

--- private/scripts/install.sh
+++ private/scripts/install.sh
@@ -1,1 +1,10 @@
-echo "Installing..."
+#! /bin/sh
+echo "Set up the target environment"
+
+# Log in, but don't output the password to Travis
+echo "Authenticating into terminus"
+{
+  terminus auth login $PEMAIL --password=$PPASS
+} &> /dev/null
+
+terminus sites list
--- private/scripts/test.sh
+++ private/scripts/test.sh
@@ -1,1 +1,6 @@
-echo "Testing..."
+#! /bin/sh
+echo "Set up behat ENV vars"
+export BEHAT_PARAMS="context[parameters][base_url]=$BASE_URL"
+export PATH="$TRAVIS_BUILD_DIR/vendor/bin:$PATH"
+
+behat

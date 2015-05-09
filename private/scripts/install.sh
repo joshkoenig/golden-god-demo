c40b252e8c018ed603070cba1a6c9d8bc0c0fcdc
Josh Koenig <josh@getpantheon.com>
2015-05-09 12:39:05 -0700

    composer install and PATH var

--- private/scripts/install.sh
+++ private/scripts/install.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 echo "Set up the target environment"
+composer install
 
+export PATH="$TRAVIS_BUILD_DIR/vendor/bin:$PATH"
 # Log in, but don't output the password to Travis
 echo "Authenticating into terminus"
 {

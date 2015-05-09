2ec17ee9457a9481ec62c1b2ebe87fe6eae0dcf1
Josh Koenig <josh@getpantheon.com>
2015-05-09 12:52:10 -0700

    testing env vars

--- private/scripts/test.sh
+++ private/scripts/test.sh
@@ -1,6 +1,14 @@
 #! /bin/sh
 echo "Set up behat ENV vars"
+
+echo "testing vars: $TRAVIS_PULL_REQUEST"
+echo $TRAVIS_PULL_REQUEST
+
+echo "testing vars: ${TRAVIS_PULL_REQUEST}"
+echo ${TRAVIS_PULL_REQUEST}
+
 export BEHAT_PARAMS="context[parameters][base_url]=$BASE_URL"
 export PATH="$TRAVIS_BUILD_DIR/vendor/bin:$PATH"
 
-behat
+echo "Running Behat tests"
+# behat

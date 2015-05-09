131877a5eaf652a83284fc1453d64bf037b67b18
Josh Koenig <josh@getpantheon.com>
2015-05-09 12:46:00 -0700

    Manual terminus install for now

--- private/scripts/install.sh
+++ private/scripts/install.sh
@@ -1,6 +1,11 @@
 #! /bin/sh
 echo "Set up the target environment"
+composer self-update
 composer install
+
+echo "Install terminus directly"
+curl https://github.com/pantheon-systems/cli/releases/download/0.5.5/terminus.phar -L -o /usr/local/bin/terminus 
+chmod +x /usr/local/bin/terminus
 
 export PATH="$TRAVIS_BUILD_DIR/vendor/bin:$PATH"
 # Log in, but don't output the password to Travis
